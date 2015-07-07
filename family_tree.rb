68844a0050980ade4dc40dc9e4d2fa11707533cd
Sarah Kwak <sarahkwak@hotmail.com>
2015-07-06 23:37:13 -0700

    grand_parent and siblings function added to the class

--- family_tree.rb
+++ family_tree.rb
@@ -38,6 +38,35 @@ module Tree
       @children.push(child)
     end
 
+    def grand_parent_name
+      raise ArgumentError, 'There is no Grand Parent' if self.parent === nil or self.parent.parent == nil
+      return parent.parent.name
+    end
+
+    def siblings
+      if @parent.child.size === 0
+        return 0
+      end
+      @parent.child.each { |each_child| p each_child.name }
+    end
+
+    def is_leaf?
+      @children.size == 0
+    end
+
+    def is_root?
+      @parent == nil
+    end
+
+    def grand_children
+      grand_children = []
+      @children.each do |child|
+        child.children.each do |g_child|
+          grand_children << g_child
+        end
+      end
+      grand_children
+    end
   end #class ends
 
 end #module ends
